66e4267aa99a9226989f6b87446448e78b822a29
ramon <ramonintouss@gmail.com>
2022-09-22 11:11:30 -0300

    .

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,4 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: "https://rocketmovies-api492.herokuapp.com"
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> f67edf5ee5962bf503cb30ab8d03bd121bcfe17b
